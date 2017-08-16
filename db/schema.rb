12fec1d3e7da19f9e99dd3159c1bb31f1e1baa57
khalid abubakr <khalid7.bkr@gmail.com>
2017-08-16 21:55:27 +0300

    removed admin remove migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816171054) do
-
+ActiveRecord::Schema.define(version: 20170816180507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,13 +88,12 @@ ActiveRecord::Schema.define(version: 20170816171054) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone_number"
+    t.boolean  "admin",                  default: false, null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "facebook_picture_url"
     t.string   "token"
     t.datetime "token_expiry"
-    t.boolean  "admin",                  default: false, null: false
-
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
