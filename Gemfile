ddc22a25b860fcbbdfee26afa1ecaf13e8a9b247
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-08-16 21:13:55 +0300

    facebook

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'cloudinary'
 gem 'carrierwave', '~> 0.11.2'
+gem 'omniauth-facebook'
 
 group :development, :test do
   gem 'pry-byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
   resources :orders
-  
+
   resources :items, only: [:index, :new, :edit, :create, :update, :show]
-  
+
   resources :pages , only: [] do
     collection do
       get :home, :menu, :dashboard, :edit_menu, :manager_orders
