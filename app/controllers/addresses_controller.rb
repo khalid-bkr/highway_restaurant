36bbd13c6f3e2b2e0685464d678d5d8adf88e7fc
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-08-16 20:23:13 +0300

    addresses controller

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,44 @@
+class AddressesController < ApplicationController
+
+  before_action :set_address, only: [:edit, :update, :destroy]
+
+  def index
+    @addresses = current_user.addresses
+  end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = current_user.addresses.new(address_params)
+    if @address.save
+      redirect_to orders_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def destroy
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+    def address_params
+      params.require(:address).permit(:city, :area, :road_name, :building_number, :apartment_number, :floor, :landmark)
+    end
+
+    def set_address
+      @address = Address.find(params[:id])
+    end
+end
