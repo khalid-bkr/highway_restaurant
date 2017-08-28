0a2a8b19a0a93bfe0a8037eb6632431ffed46c30
khalid abubakr <khalid7.bkr@gmail.com>
2017-08-28 10:15:53 +0300

    skip auth for menu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :contact_us]
+  skip_before_action :authenticate_user!, only: [:home, :contact_us, :menu]
 
   def home
   end
