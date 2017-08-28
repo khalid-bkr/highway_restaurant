004b2c0b3d4a27886b80dc1d58a0463cde337129
khalid abubakr <khalid7.bkr@gmail.com>
2017-08-28 11:32:35 +0300

    disabled comments in active admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -10,6 +10,8 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = "Highway Restaurant"
 
+
+  config.comments = false
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
